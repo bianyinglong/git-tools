069d041d7a05ca75c8753f6e184e8ed64d763121
bianyinglong <1715106831@qq.com>
2024-08-09 17:39:31 +0800

    feat: 开发gpush

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import { execa } from 'execa';
+import { program } from 'commander';
 import { resolveCurrentBranch } from './command/review.js'
+
 
 
 resolveCurrentBranch().then(res => {
@@ -7,3 +10,45 @@ resolveCurrentBranch().then(res => {
     console.log('err=>', err);
 })
 
+
+const gitPush = async (branchName, cwd) => {
+    console.log('git push ...', 'CR');
+
+    const res = await execa(
+        'git',
+        ['push', 'origin', `head:refs/for/${branchName}`],
+        { cwd: cwd }
+    );
+
+    const content = res.stderr;
+
+    console.log(content);
+
+    if (res.exitCode !== 0) {
+        process.exit(1)
+    }
+
+    console.log(`${branchName} 分支已推送至远端`);
+
+    return content;
+}
+
+const createCodeReview = async () => {
+    const cwd = process.cwd();
+    const branchName = await resolveCurrentBranch(cwd);
+    const content = await gitPush(branchName, cwd);
+}
+
+// program.command();
+// console.log(program.parse(process.argv))
+
+program
+    .command('gpush')
+    .description('发送 CR 请求')
+    .action(async () => {
+        createCodeReview()
+    });
+
+program.command
+
+program.parse(process.argv)
